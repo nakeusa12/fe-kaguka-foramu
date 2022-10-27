e46a98e3e8f17773d7a31a3da9ac418aed88ae3a
nakeusa12 <snakeusa8@gmail.com>
2022-10-27 23:21:15 +0700

    routes and guardRoute

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Homepage } from "./pages/Homepage";
-import { Login } from "./pages/Login";
-import { Dashboard } from "./pages/Dashboard";
-import { Categories } from "./pages/Categories";
-import CategoryCreate from "./pages/Categories/create";
-import CategoryEdit from "./pages/Categories/edit";
-import ComponentNavbar from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 import { listen } from "./redux/listener";
 import { useEffect } from "react";
+import { AppRoutes } from "./routes";
 
 function App() {
   useEffect(() => {
     listen();
   }, []);
   return (
-    <>
-      <Router>
-        <ComponentNavbar />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/categories/create" element={<CategoryCreate />} />
-          <Route path="/categories/edit/:id" element={<CategoryEdit />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <AppRoutes />
+    </Router>
   );
 }
 

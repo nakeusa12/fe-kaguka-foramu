bef6221ad03ff627a2c19ca6d2f879b1a62c6491
nakeusa12 <snakeusa8@gmail.com>
2022-10-27 22:42:35 +0700

    fix

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -1,22 +1,17 @@
 import { USER_LOGIN, USER_LOGOUT } from './constants';
 
-let initialState = localStorage.getItem('auth')
-  ? JSON.parse(localStorage.getItem('auth'))
-  : { token: null, role: null, refreshToken: null };
+export function userLogin(token, role, refreshToken) {
+  return {
+    type: USER_LOGIN,
+    token,
+    role,
+    refreshToken,
+  };
+}
 
-export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case USER_LOGIN:
-      return {
-        token: action.token,
-        role: action.role,
-        refreshToken: action.refreshToken,
-      };
-
-    case USER_LOGOUT:
-      return { token: null, role: null, refreshToken: null };
-
-    default:
-      return state;
-  }
+export function userLogout() {
+  localStorage.removeItem('auth');
+  return {
+    type: USER_LOGOUT,
+  };
 }
